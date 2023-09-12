434dca7b906e572ecae20c7060dd3195d9779074
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-12 15:14:54 +0800

    created vitaminc scrapy project

--- vitamincscraper/vitamincscraper/spiders/vitamincspider.py
+++ vitamincscraper/vitamincscraper/spiders/vitamincspider.py
@@ -1,8 +1,8 @@
 import scrapy
 
 
-class MooncakespiderSpider(scrapy.Spider):
-    name = "mooncakespider"
+class VitamincspiderSpider(scrapy.Spider):
+    name = "vitamincspider"
     allowed_domains = ["https://sg.iherb.com"]
     start_urls = ["https://sg.iherb.com/c/vitamin-c"]
 
@@ -14,14 +14,3 @@ class MooncakespiderSpider(scrapy.Spider):
                   'rating_no' : p.css('div:nth-of-type(2) > a:nth-of-type(2) > span::text').extract(),
                   'price' : p.css('div:nth-of-type(5) > .product-price-top > div > span > bdi::text').extract()
             }
-
-    # name = "lovespider"
-    # allowed_domains = ["www.lovebonito.com"]
-    # start_urls = ["https://www.lovebonito.com/sg/shop/apparel-accessories/dresses?stock.is_in_stock=true"]
-
-    # def parse(self, response):
-    #     product = response.css('div.sf-product-card')
-    #     for p in product:
-    #         yield{'name' : p.css('h3.sf-product-card__title::text').get().replace('\n',''),
-    #               'price' : p.css('div.sf-product-card__price::text').get().replace('\n','')
-    #         }
