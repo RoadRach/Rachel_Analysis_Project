d45ccc25800a9aecba3497e11bfe2e1797baa539
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-12 12:31:29 +0800

    add notes

--- mooncakescraper/mooncakescraper/spiders/mooncakespider.py
+++ mooncakescraper/mooncakescraper/spiders/mooncakespider.py
@@ -4,7 +4,7 @@ import scrapy
 class MooncakespiderSpider(scrapy.Spider):
     name = "mooncakespider"
     allowed_domains = ["shopee.sg"]
-    start_urls = ["https://shopee.sg"]
+    start_urls = ["https://shopee.sg/search?keyword=mooncake"]
 
     def parse(self, response):
         pass
