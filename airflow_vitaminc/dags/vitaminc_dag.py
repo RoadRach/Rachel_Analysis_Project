d0cb8bfd001b90464f57014261b088f126bd242c
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-28 18:46:00 +0800

    airflow vitamin c

--- airflow_vitaminc/dags/vitaminc_dag.py
+++ airflow_vitaminc/dags/vitaminc_dag.py
@@ -1,10 +1,27 @@
 from airflow import DAG 
-from datetime import datetime
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+default_args = {
+    'owner': 'rach',
+    'retries' : 5,
+    'retry_delay' : timedelta(minute=2)
+}
 
 with DAG(
     dag_id='our_first_dag',
+    default_args=default_args,
     description='This is my first dag',
     start_date=datetime(2023,9,13,2),
     schedule_interval='@hourly'
 ) as dag:
-    pass
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo hello world"
+    )
+    task2 = PythonOperator(
+        task_id='second_task',
+        
+    )
+
+    task1
