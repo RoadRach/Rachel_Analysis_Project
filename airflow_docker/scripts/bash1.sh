13f38f3677cb75a5a29b7fc0093e51a1cce95789
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-29 16:18:44 +0800

    output file with date and time to file name

--- airflow_docker/scripts/bash1.sh
+++ airflow_docker/scripts/bash1.sh
@@ -2,5 +2,5 @@ echo "The script is starting"
 
 cd /opt/airflow/vitamincscrapy/ && pwd
 
-scrapy crawl vitamincspider -O airflowscrapy.csv
+scrapy crawl vitamincspider -O Output_Data/airflowscrapy-$(date '+%FT%T.%3N').csv
 echo "Script executed"
