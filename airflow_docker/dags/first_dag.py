a5a2ee85ce9c7172c00bbae4ade944ac7dea98fa
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-29 19:31:00 +0800

    connection with postgres db

--- airflow_docker/dags/first_dag.py
+++ airflow_docker/dags/first_dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from datetime import datetime,timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-# from airflow_docker.vitamincscraper.vitamincscraper.spiders.vitamincspider import VitamincspiderSpider
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 default_args = {
     'owner': 'rachang',
@@ -15,11 +15,23 @@ with DAG(
     default_args=default_args,
     description='this is first dag',
     start_date=datetime(2023,9,29),
-    schedule_interval='@daily'
+    schedule_interval='0 0 * * *'
 ) as dag:
     task1 = BashOperator(
         task_id='first_task',
-        bash_command="/opt/airflow/scripts/bash1.sh "
+        bash_command="/opt/airflow/scripts/shell/bash1.sh "
+    )
+
+    task2 = PostgresOperator(
+        task_id='create_postgres_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TABLE IF NOT EXISTS dag_runs(
+            dt date,
+            dag_id character varying,
+            primary key (dt, dag_id)
+            )
+        """
     )
 
     # task2 = PythonOperator(
@@ -27,4 +39,4 @@ with DAG(
     #     python_callable=VitamincspiderSpider
     # )
 
-    task1
+    task2
